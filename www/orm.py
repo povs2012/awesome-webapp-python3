2f7857f0a6fe6189bfa9f8ca3efbf1ccbcceb292
guofuming <guofuming@outlook.com>
2015-09-16 23:17:41 +0800

    modify orm

--- www/orm.py
+++ www/orm.py
@@ -54,3 +54,21 @@ def execute(sql, args):
         except BaseException as e:
             raise
         return affected
+
+
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.name, self.column_type, self.name)
