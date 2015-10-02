c95feb721b1485de85cb2e9127e88c93e2e208c7
guofuming <guofuming@outlook.com>
2015-10-02 11:39:14 +0800

    modify app.py

--- www/app.py
+++ www/app.py
@@ -133,7 +133,7 @@ def datetime_filter(t):
     if delta < 86400:
         return u'%s小时前' % (delta // 3600)
     if delta < 604800:
-        return u'%s天前' % (delta // 604800)
+        return u'%s天前' % (delta // 86400)
     dt = datetime.fromtimestamp(t)
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
